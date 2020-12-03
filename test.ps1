986b1167e425c48b3c56780fc0c557e7ed828aa4
Suz Hinton <54524269+suz-stripe@users.noreply.github.com>
2020-12-02 18:46:35 -0800

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -3,5 +3,5 @@ Set-ExecutionPolicy RemoteSigned -scope CurrentUser
 # install scoop
 Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
 
-scoop bucket add test-bucket https://raw.githubusercontent.com/suz-stripe/scoop-test/main/app_manifest.json
+scoop bucket add test-bucket https://suz-stripe.github.io/scoop-test/app_manifest.json
 scoop install hello
